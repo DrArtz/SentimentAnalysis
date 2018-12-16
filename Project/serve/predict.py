65d1b6e5998cb640cba62e6fa203a36158d0279c
EC2 Default User <ec2-user@ip-172-16-47-234.us-east-2.compute.internal>
2018-12-16 21:48:59 +0000

    finish prediction and model training

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -70,8 +70,7 @@ def predict_fn(input_data, model):
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
 
-    data_X = None
-    data_len = None
+    data_X, data_len = convert_and_pad(model.word_dict, input_data)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -86,7 +85,20 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
-    result = None
+    result = model.forward(data)
 
     return result
+
+def convert_and_pad(word_dict, sentence, pad=500):
+    NOWORD = 0 # We will use 0 to represent the 'no word' category
+    INFREQ = 1 # and we use 1 to represent the infrequent words, i.e., words not appearing in word_dict
+    
+    working_sentence = [NOWORD] * pad
+    
+    for word_index, word in enumerate(sentence[:pad]):
+        if word in word_dict:
+            working_sentence[word_index] = word_dict[word]
+        else:
+            working_sentence[word_index] = INFREQ
+            
+    return working_sentence, min(len(sentence), pad)
